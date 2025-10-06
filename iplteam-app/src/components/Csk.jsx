a98e5aca0110b376df0ad0f895b230c31df3f348
Abhijeet narsale <abhijeetnarsale14082004@gmail.com>
2025-10-06 18:40:39 +0530

    Using Routing

--- iplteam-app/src/components/Csk.jsx
+++ iplteam-app/src/components/Csk.jsx
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom'
 
 const Csk = () => {
     return (
-        <div className='text-center text-6xl bg-yellow-500 justify-center items-center'> <div className='flex text-center text-6xl bg-yellow-500 justify-center items-center gap-7'>Chennai Super Kings <img src="https://th.bing.com/th/id/OSK.pVH83_irFUd8pOcKJXgvAky5ClU2ysdbwg0l5Ylr44E?w=102&h=102&c=7&o=6&dpr=1.3&pid=SANGAM" alt="" height={40} width={50} /> </div>
-       
-        <div className='h-[100%] border-red-500 bg-yellow-200 text-6xl justify-center items-center space-y-4 flex gap-20'>
-           
-            <img src="https://c.ndtvimg.com/2023-05/k8lfjlto_ms-dhoni_625x300_29_May_23.jpg?im=FaceCrop" alt="" height={500} width={600} />
-            <img src="https://img.jagranjosh.com/images/2023/May/3052023/ipl-2023-winner-chennai-super-kings-csk.webp" alt="" height={500} width={600}/> 
-</div>
-          
-          <div className='justify-center items-center text-center flex gap-20 bg-orange-500'>  <img src="https://www.hindustantimes.com/ht-img/img/2023/04/04/1600x900/PTI04-03-2023-000365A-0_1680606588325_1680606588325_1680606904951_1680606904951.jpg" alt="" height={500} width={600}/>
-            <img src="https://th.bing.com/th/id/OIP.coIVE0d-FDgQtQVfNd9YGgHaEK?w=322&h=181&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="" height={500} width={600}/>
-        </div>
+        <div className='text-center text-6xl bg-yellow-500 justify-center items-center'>
+            <div className='flex text-center text-6xl bg-yellow-500 justify-center items-center gap-7'>Chennai Super Kings
+                <img src="https://th.bing.com/th/id/OSK.pVH83_irFUd8pOcKJXgvAky5ClU2ysdbwg0l5Ylr44E?w=102&h=102&c=7&o=6&dpr=1.3&pid=SANGAM" alt="" height={40} width={50} /> </div>
+
+            <div className='h-[100%] border-red-500 bg-yellow-200 text-6xl justify-center items-center space-y-4 flex gap-20'>
+
+                <img src="https://c.ndtvimg.com/2023-05/k8lfjlto_ms-dhoni_625x300_29_May_23.jpg?im=FaceCrop" alt="" height={500} width={600} />
+                <img src="https://img.jagranjosh.com/images/2023/May/3052023/ipl-2023-winner-chennai-super-kings-csk.webp" alt="" height={500} width={600} />
+            </div>
+
+            <div className='justify-center items-center text-center flex gap-20 bg-orange-500'>  <img src="https://www.hindustantimes.com/ht-img/img/2023/04/04/1600x900/PTI04-03-2023-000365A-0_1680606588325_1680606588325_1680606904951_1680606904951.jpg" alt="" height={500} width={600} />
+                <img src="https://th.bing.com/th/id/OIP.coIVE0d-FDgQtQVfNd9YGgHaEK?w=322&h=181&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="" height={500} width={600} />
+            </div>
         </div>
     )
 }

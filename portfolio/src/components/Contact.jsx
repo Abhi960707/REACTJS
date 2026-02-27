e51065585d15171eb227f718df5d2c19f75c7c27
Abhijeet Narsale <abhijeetnarsale14082004@gmail.com>
2026-02-27 23:30:40 +0530

    Changes the Contact

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,20 +1,67 @@
 import React from 'react'
 
+
 const Contact = () => {
   return (
-    <section id="contact" className="py-20 px-6 max-w-4xl mx-auto text-center">
-      <h2 className="text-4xl font-bold mb-8">Contact Me</h2>
+    <section id="contact" className="py-20 px-6from-gray-900 to-black text-white">
+      <div className="max-w-5xl mx-auto text-center">
+        <h2 className="text-4xl font-bold mb-8">Contact Me</h2>
 
-      <p className="text-white-400 mb-6">
-        Feel free to reach out for collaboration or job opportunities.
-      </p>
+        <p className="text-white-400 mb-4">
+          I'm Open to collaboration and meaningful opportunities where I can contribute, learn, and grow.
+        </p>
 
-      <a
-        href="mailto:abhijeetnarsale14082004@gmail.com"
-        className="bg-blue-600 px-6 py-3 rounded-lg hover:bg-blue-700 transition"
-      >
-        Send Email
-      </a>
+        {/* My contact All Details */}
+        <div className="grid md:grid-cols-3 gap-6 mb-12">
+
+          <div className="bg-gray-800 p-6 rounded-2xl shadow-lg hover:scale-105 transition">
+            <h3 className="text-xl font-semibold mb-2"> Email</h3>
+            <a
+              href="mailto:abhijeetnarsale14082004@gmail.com"
+              className="text-blue-400 hover:underline"
+            >
+              abhijeetnarsale14082004@gmail.com
+            </a>
+          </div>
+
+          <div className="bg-gray-800 p-6 rounded-2xl shadow-lg hover:scale-105 transition">
+            <h3 className="text-xl font-semibold mb-2"> LinkedIn</h3>
+            <a
+              href="https://www.linkedin.com/in/abhijeet-narsale-a889832a3"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-400 hover:underline"
+            >
+              View Profile
+            </a>
+          </div>
+
+          <div className="bg-gray-800 p-6 rounded-2xl shadow-lg hover:scale-105 transition">
+            <h3 className="text-xl font-semibold mb-2"> GitHub</h3>
+            <a
+              href="https://github.com/Abhi960707"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-400 hover:underline"
+            >
+              Visit GitHub
+            </a>
+          </div>
+
+        </div>
+
+        {/* This is Resume Button */}
+        <div className="mt-8">
+          <a
+            href="/Abhijeet Narsale Cv.pdf"
+            download
+            className="border border-amber-500 px-6 py-3 rounded-lg font-medium hover:bg-green-900  cursor-pointer "
+          >
+            Download Resume 📄
+          </a>
+        </div>
+
+      </div>
     </section>
   );
 };
--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -9,15 +9,23 @@ const Projects = () => {
 
         <div className="bg-gray-900 p-6 rounded-lg">
           <h3 className="text-xl font-semibold text-blue-400">
-            Sport Academy Management System
+            <a
+              href="https://frontend-rho-pink-86.vercel.app"
+              target="_blank"
+              rel="noopener noreferrer"
+
+            >
+              Sport Academy Management System
+            </a>
           </h3>
           <p className="mt-3 text-gray-400">
             MERN based application for managing players and events.
           </p>
+
         </div>
 
         <div className="bg-gray-900 p-6 rounded-lg">
-          <h3 className="text-xl font-semibold text-blue-400">
+          <h3 className="text-xl font-semibold text-blue-400 cursor-pointer">
             Pharmacy Management System
           </h3>
           <p className="mt-3 text-gray-400">
@@ -26,8 +34,8 @@ const Projects = () => {
         </div>
 
         <div className="bg-gray-900 p-6 rounded-lg">
-          <h3 className="text-xl font-semibold text-blue-400">
-            Online Campus Recruitment
+          <h3 className="text-xl font-semibold text-blue-400 cursor-pointer">
+            Online Campus Recruitment System
           </h3>
           <p className="mt-3 text-gray-400">
             ASP.NET based platform for job postings and student accounts.

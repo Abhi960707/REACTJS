e061ecc6e4b114e017bc5923346132dd2fc0bb6c
Abhijeet Narsale <abhijeetnarsale14082004@gmail.com>
2026-07-21 18:50:21 +0530

    update some information

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
             <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
           </span>
-          Available for Full-Time Roles & Internships
+          🚀 Open to Full-Time Software Developer Opportunities
         </span>
       </motion.div>
 
